60da1bd045e605381f057724333bccaab3246b52
Chance <phs811@naver.com>
2022-01-24 17:12:56 +0900

    복습 ViewModel

--- app/src/main/java/com/example/mvvm_practice/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_practice/MainActivity.kt
@@ -13,18 +13,42 @@ import androidx.room.Room
 import com.example.mvvm_practice.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() , View.OnClickListener{
 
+    companion object{
+        const val TAG : String = "로그"
+    }
+
+    lateinit var myNumberViewModel: MyNumberViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        // 뷰 모델 가져오기
-        val viewModel : CounterViewModel = ViewModelProvider(this).get(CounterViewModel::class.java)
 
-        // 변경된 값을 찾아서 알아서 refesh해줌
-        binding.setLifecycleOwner(this)
-        binding.setViewModel(viewModel)
+        myNumberViewModel = ViewModelProvider(this).get(MyNumberViewModel::class.java)
+
+        // 뷰 모델 라이브 데이터 접근
+        myNumberViewModel.currentValue.observe(this , Observer {
+            Log.d(TAG, "Main - ViewModel - currentValue 라이브 데이터 값 변경 : ${it}")
+            textView.text = it.toString()
+        })
+
+        plus_btn.setOnClickListener(this)
+        minus_btn.setOnClickListener(this)
+
+    }
+
+    // 클릭 이벤트
+    override fun onClick(view: View?) {
+        val userInput = userinput_edittext.text.toString().toInt()
+
+        when(view){
+            plus_btn -> {
+                myNumberViewModel.updateValue(actionType = ActionType.PLUS , userInput)
+            }
+            minus_btn -> {
+                myNumberViewModel.updateValue(actionType = ActionType.MINUS , userInput)
+            }
+        }
 
     }
 }
